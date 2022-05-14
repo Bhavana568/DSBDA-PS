5cc3f22f05e7652653612844cfb3cd5b0684b1b4
Bhavana568 <bhavnabafna.bb@gmail.com>
2022-05-15 00:39:52 +0530

    added files

---
+++ Hadoop/MaxUserCount.java
@@ -0,0 +1,68 @@
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class MaxCount {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    //private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    int v;
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+    	  word.set(itr.nextToken());  
+    	  v=Integer.parseInt(itr.nextToken());
+    	  context.write(word, new IntWritable(v));
+      }
+    }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    //private IntWritable result = new IntWritable();
+	  static Text key1=new Text();
+	  int maxtime=0;
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      
+      for (IntWritable val : values) {
+        if(maxtime<val.get()){
+        	maxtime=val.get();
+        	key1.set(key);
+        }
+      }
+      context.write(key1,new IntWritable(maxtime));
+      
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(MaxCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
